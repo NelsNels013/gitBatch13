5ad49bc18431a3785d928e254290d30767f61727
NelsNels013 <Nelsonlg013@gmail.com>
2022-08-24 19:36:21 -0400

    first commit

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class gitBasics {
+
+    //write down a code that checks the temperature from a website and then
+    //prints out that is a beautiful day or not 
+    public static void main(String[] args) {
+        System.out.println("today is a beautiful day");
+    }
+}
